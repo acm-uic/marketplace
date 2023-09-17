2265caf62d6531ea832007fd04eea4f267bacebe
sam e <49312226+dejazzhands@users.noreply.github.com>
2023-09-17 11:06:46 -0500

    Update UserCard.jsx
    added my usercard

--- app/components/UserCard.jsx
+++ app/components/UserCard.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import profilePic from "/app/images/MilkFaceCatGif.gif"
+import anotherProfile from "/app/images/dog.jpg"
 import Link from "next/link";
 
 //TODO set min width
@@ -33,6 +34,24 @@ export const FakeNamer = () => {
           </h3>
           <div>
             <Link href={"https://github.com/Ajknight121"}>@Ajknight121</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export const SamuelE = () => {
+  return (
+    <div>
+      <div className="flex justify-center align-middle bg-sky-900 w-300px h-60px m-5 border-solid border-2 border-blue-600" >
+        <Image src={profilePic} width={60} height={60} alt="profile picture of Samuel E"/>
+        <div className="text-white text-center w-full self-center">
+          <h3 className="font-2xl">
+            Samuel E
+          </h3>
+          <div>
+            <Link href={"https://github.com/dejazzhands"}>dejazzhands</Link>
           </div>
         </div>
       </div>
