76a6721c1445907bcc42c55aeae8c0cbbf44f301
Andre Rimes <andre.2004.rimes@gmail.com>
2023-10-01 16:29:06 -0500

    size ajust
    font, margin and size modification

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -6,14 +6,13 @@ import cart from '../images/cart.png'
 import sun from "../images/sun.png"
 import moon from "../images/moon.png"
 
-export default function Header({isDarkMode,setIsDarkMode}) {
-
+export default function Header({ isDarkMode, setIsDarkMode }) {
     return (
-        <div className="flex items-center justify-between " >
+        <div className="flex items-center justify-between">
             <div className="font-bold text-lg mt-4">
-                <Link className="text-xl ml-6 mr-10 text-white hover:underline" href="/">Shop</Link>
-                <Link className="text-xl mr-10 text-white hover:underline" href="https://acm.uic.edu/about">About</Link>
-                <Link className="text-xl text-white  hover:underline" href='https://acm.uic.edu/about'>Contact</Link>
+                <Link className="text-2xl ml-6 mr-10 text-white hover:underline" href="/">Shop</Link>
+                <Link className="text-2xl mr-10 text-white hover:underline" href="https://acm.uic.edu/about">About</Link>
+                <Link className="text-2xl text-white hover:underline w" href='https://acm.uic.edu/about'>Contact</Link>
             </div>
             <div className="mt-4">
                 <Link href="https://acm.uic.edu/">
@@ -21,11 +20,11 @@ export default function Header({isDarkMode,setIsDarkMode}) {
                 </Link>
             </div>
 
-            <div className="flex flex-row mt-4 text-black"  style={{maxWidth:"283px"}}>
-                <input placeholder="Search" className="rounded-lg w-56 pl-2 mt-1 mr-4" style={{ height: '29px' }}  type="text"/>
-                <Image onClick={()=>setIsDarkMode(!isDarkMode)} src={isDarkMode? sun: moon} width={16} height={16} className="mr-2"/>
-                <Link href="/cart" >
-                    <Image  src={cart} width={60} height={50} alt="cart" className="mr-4" />
+            <div className="flex items-center flex-row mt-4 text-black">
+                <input placeholder="Search" className="rounded-lg  w-72 pl-2 mt-1 mr-4 h-10" type="text" />
+                <Image onClick={() => setIsDarkMode(!isDarkMode)} src={isDarkMode ? sun : moon} width={28} height={28} className="mr-2" />
+                <Link href="/cart">
+                    <Image src={cart} width={60} height={50} alt="cart" className="mr-4" />
                 </Link>
             </div>
         </div>
