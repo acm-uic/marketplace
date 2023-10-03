91cf0f990acb61867754ac4c2e60b33d30b30185
floatsinkfc <floatsinkfc@gmail.com>
2023-10-02 21:44:39 -0500

    Merge branch 'main' of https://github.com/acm-uic/marketplace into zijunw-contribute

---
+++ app/components/hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function HeroScre() {
+  return (
+    <div className="flex flex-col items-center justify-center max-h-screen bg-cover bg-center">
+      <h1 className="text-white text-9xl font-extrabold mb-8 mt-72 text-shadow-hero shadow-HEROSHADOW">ACM Merch</h1>
+      <button className="bg-white px-4 py-4 rounded-full ">
+        <p className="text-center text-ACMPrimary font-semibold text-4xl text-shadow-hero shadow-SHOPTEXTSHADOW">SHOP NOW</p>
+      </button>
+    </div>
+  );
+}
