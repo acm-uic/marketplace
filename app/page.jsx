2930b0911b674fbaf56a59881f1e5f95bbaf5b00
floatsinkfc <floatsinkfc@gmail.com>
2023-10-02 22:13:49 -0500

    changed the theme to match Figma

--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,11 @@ import HeroScreen from "./components/hero"
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [color, setColor] = useState("linear-gradient(180deg, #D90000 0%, #242526 100%)");
+  const [color, setColor] = useState("linear-gradient(180deg, #D90000 88.89%, #FFF 100%)");
   const [isDarkMode, setIsDarkMode] = useState(true)
  
   return (
-    <div className="w-full h-full text-white" style={{ background: isDarkMode?"linear-gradient(180deg, #D90000 0%, #242526 100%)":"#E6AF2E" }}>
+    <div className="w-full h-full text-white" style={{ background: isDarkMode?"linear-gradient(180deg, #D90000 88.89%, #FFF 100%)":"#34A3D9" }}>
       <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
       <HeroScreen/>
     </div>
