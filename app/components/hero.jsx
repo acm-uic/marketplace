4802dfc298d88bb86e3504fa7f2a83694762e68a
floatsinkfc <floatsinkfc@gmail.com>
2023-10-02 22:17:06 -0500

    fixed the layout of the button

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -4,8 +4,8 @@ export default function HeroScre() {
   return (
     <div className="flex flex-col items-center justify-center max-h-screen bg-cover bg-center">
       <h1 className="text-white text-9xl font-extrabold mb-8 mt-72 text-shadow-hero shadow-HEROSHADOW">ACM Merch</h1>
-      <button className="bg-white px-4 py-4 rounded-full ">
-        <p className="text-center text-ACMPrimary font-semibold text-4xl text-shadow-hero shadow-SHOPTEXTSHADOW">SHOP NOW</p>
+      <button className="bg-white px-5 py-6 rounded-full ">
+        <p className="text-center text-ACMPrimary font-bold text-4xl text-shadow-hero shadow-SHOPTEXTSHADOW">SHOP NOW</p>
       </button>
     </div>
   );
