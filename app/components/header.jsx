51f2ef1ec0bd6d3bf5655e76b96c770d72be1d6f
floatsinkfc <floatsinkfc@gmail.com>
2023-10-06 00:04:04 -0500

    fixed header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -8,9 +8,8 @@ import sun from "../images/sun.png"
 import moon from "../images/moon.png"
 
 export default function Header({ isDarkMode, setIsDarkMode, invertedColor }) {
-    const colorHeader = invertedColor ? "invert" : "invert-0";
     return (
-        <div className={`flex items-center justify-between  w-full absolute ${colorHeader}`}>
+        <div className={`flex items-center justify-between  w-full absolute`}>
             <div className="font-bold text-lg mt-4">
                 <Link href="/" className="text-2xl ml-6 mr-10 text-white hover:underline">Shop</Link>
                 <Link className="text-2xl mr-10 text-white hover:underline" href="https://acm.uic.edu/about" target="_blank">About</Link>
