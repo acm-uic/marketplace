0cd82a951616d82d65449ede8410143a8c582308
Andre Rimes <andre.2004.rimes@gmail.com>
2023-10-10 21:29:03 -0500

    pull main into the branch
    pull main into the branch

--- app/page.jsx
+++ app/page.jsx
@@ -4,22 +4,28 @@ import HeroScreen from "./components/hero"
 import ProductList from "./components/productlist";
 import { useState, useRef, useEffect } from "react";
 import { useTheme } from "./components/ThemeContext";
+import listOfProduct from './testData/produts';
+
 
 export default function Home() {
   const { isDarkMode } = useTheme();
   const productListRef = useRef(null);
+  const [productList, setProductList] = useState([])
+
   
 
-  const scrollToProductList = () => { //Scroll Animation
-    productListRef.current.scrollIntoView({ 
-    behavior: "smooth",
-    });
+  const scrollToProductList = () => { //Scroll \\\\
+    productList.current.scrollIntoView({ behavior: "smooth" });
   };
+  useEffect(() => {
+    // change this to fetch data
+    setProductList(listOfProduct)
+  }, [productList])
 
   return ( 
     <div className="w-full h-full text-white" >
       <HeroScreen scrollToProductList={scrollToProductList} isDarkMode={isDarkMode}/>
-      <ProductList productListRef={productListRef} isDarkMode={isDarkMode}/>
+      <ProductList productList={productList} isDarkMode={isDarkMode} />
       
     </div>
 
