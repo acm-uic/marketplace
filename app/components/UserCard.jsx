40052871ec6c609931757c305bdbfa5cd34d71ed
Ajknight121 <aknig2@uic.edu>
2023-10-11 13:46:42 -0500

    Re-added my pfp ping effect

--- app/components/UserCard.jsx
+++ app/components/UserCard.jsx
@@ -14,7 +14,7 @@ export const AdrianKnight = ( ) => {
   return (
     <div>
       <div className="flex justify-center align-middle bg-white shadow-contributor max-w-[380px] h-[100px] m-5 border-solid border-2 border-black" >
-        <Image className="rounded-[50%] border-indigo-600 border-[4px] -translate-x-[20%] -translate-y-4" src={profilePicAdrian} width={100} height={100} alt="profile picture of Adrian Knight" />
+        <Image className="rounded-[50%] border-indigo-600 border-[4px] -translate-x-[20%] -translate-y-4 hover:animate-ping delay-1000" src={profilePicAdrian} width={100} height={100} alt="profile picture of Adrian Knight" />
         <div className="text-black text-center w-full self-center mr-5">
           <h3 className="text-2xl  ">
             Adrian Knight
