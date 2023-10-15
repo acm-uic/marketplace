514ae28448d379bb59bc470f8a782105d6acc259
floatsinkfc <floatsinkfc@gmail.com>
2023-10-14 20:37:34 -0500

    revert changes

--- app/components/product.jsx
+++ app/components/product.jsx
@@ -14,7 +14,7 @@ export default function Product({ id, title, price, imgUrl,isDarkMode}) {
       className={`font-bold bg-white text-black p-5 rounded-lg relative w-52 h-60 shadow-[12px_13px_6px_0_rgba(36,37,38,0.25)] 
       ${hovered?"border-solid border-ACMLIGHT  border-2":""} `}
     >
-      <Image alt="" src={imgUrl} width={180} height={200} className="h-36" />
+      <Image src={imgUrl} width={180} height={200} className="h-36" />
       <div
         className={`bg-white flex items-center flex-col justify-center absolute w-44 cursor-pointer rounded-lg transition-all duration-200 text-ACMDARK	ease-in	
         ${
--- app/components/productFilter.jsx
+++ app/components/productFilter.jsx
@@ -1,12 +1,10 @@
-import Link from "next/link";
-
 //Content just to test position of the product List
 export default function ProductFilter({isDarkMode}){
     return(
-        <div className={`${isDarkMode? 'text-white': 'text-ACMDARK'} flex flex-row w-screen justify-center lg:text-[28px] font-[500] tracking-[7.83px]  lg:gap-[182px] sm:gap-[100px] sm:text-[20px] mr-[2.45rem] `}>
-        <Link className="hover:text-ACMLIGHT" href="/">Stickers</Link>
-        <Link className="hover:text-ACMLIGHT" href="/">Pen</Link>
-        <Link className="hover:text-ACMLIGHT" href="/">Shirts</Link>
+        <div className={`${isDarkMode? 'text-white': 'text-ACMDARK'} flex flex-row`}>
+        <h1>Category 1</h1>
+        <h1>Category 2</h1>
+        <h1>Category 3</h1>
         </div>
     )
 }
