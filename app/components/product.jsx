d48cc7f6df1bd1ee76a633977518be661196ff1e
Ajknight121 <aknig2@uic.edu>
2023-10-18 17:51:14 -0500

    consistent product component size

--- app/components/product.jsx
+++ app/components/product.jsx
@@ -11,8 +11,8 @@ export default function Product({ id, title, price, imgUrl,isDarkMode}) {
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
       key={id}
-      className={`font-bold bg-white text-black p-5 rounded-lg relative w-52 h-60 shadow-[12px_13px_6px_0_rgba(36,37,38,0.25)] 
-      ${hovered?"border-solid border-ACMLIGHT  border-2":""} `}
+      className={`box-content font-bold bg-white text-black p-5 rounded-lg relative w-52 h-60 shadow-[12px_13px_6px_0_rgba(36,37,38,0.25)] 
+      ${hovered?"border-solid border-ACMLIGHT  border-2":"border-solid border-white  border-2"} `}
     >
       <Image alt={title} src={imgUrl} width={180} height={200} className="h-36" />
       <div
