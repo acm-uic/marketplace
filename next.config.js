cbd7832d8606e7157f51c4a0c7ba5a14ad9bca8f
Ajknight121 <aknig2@uic.edu>
2023-11-06 15:26:10 -0600

    Add alt prop to usercards, add image fetching

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone'
-}
 
-module.exports = nextConfig
+module.exports = {
+  // for security outside sources of images must be specified TODO make the domain be the pocketbase ip
+  images: {
+    domains: ['127.0.0.1'],
+  },
+ }
