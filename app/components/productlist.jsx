82f5251ef5ab9b481b66879c3dc8e25e401abd53
Andre Rimes <andre.2004.rimes@gmail.com>
2023-11-15 20:49:05 -0600

    Fix Bugs
    -Center ACM Logo in the header
    -Center Image in the product component
    -Aligned the product grid

--- app/components/productlist.jsx
+++ app/components/productlist.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Product from "./product";
 import ProductFilter from "./productFilter";
 import { useProducts } from "../Domain/ProductContext";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function ProductList({productList, isDarkMode }) {
   const {products} = useProducts();
@@ -12,27 +12,28 @@ export default function ProductList({productList, isDarkMode }) {
   return (
     <div
       ref={productList} 
-      className={` ${isDarkMode? 'bg-ACMDARK': "bg-white"} text-white flex flex-col items-center  justify-center min-h-screen bg-cover bg-center w-full`} >
-      <h1 className={`text-4xl bold ${isDarkMode? 'text-white' : 'text-ACMDARK'}`}  >Products</h1>
+      className={` ${isDarkMode ? 'bg-ACMDARK' : "bg-white"} text-white flex flex-col items-center  justify-center min-h-screen bg-cover bg-center w-full`} >
+      <h1 className={`text-4xl bold ${isDarkMode ? 'text-white' : 'text-ACMDARK'}`}  >Products</h1>
       <div className="flex flex-col items-center justify-center	mt-14">
-        <ProductFilter page={page} setPage={setPage} isDarkMode={isDarkMode}/>
-        <div className="flex items-start content-start gap-x-16 gap-y-7 shrink-0 flex-wrap max-w-6xl mb-10 mt-10">
-          {products.map((product) => {
-            if (page == "All") {
-              return (
-                <div key={product.id}>
-                  <Product isDarkMode={isDarkMode} product={product}/>
-                </div>
-              )
-            }
-            else if (product.tag == page) {
-              return (
-                <div key={product.id}>
-                  <Product isDarkMode={isDarkMode} product={product}/>
-                </div>
-              )}
-          })}
-        </div>
+        <ProductFilter page={page} setPage={setPage} isDarkMode={isDarkMode} />
+          <div className="flex justify-center flex-wrap grid-cols-4 gap-x-16 gap-y-7 mt-10">
+            {products.map((product) => {  
+              if (page == "All") {
+                return (
+                  <div key={product.id}>
+                    <Product isDarkMode={isDarkMode} product={product} />
+                  </div>
+                )
+              }
+              else if (product.tag == page) {
+                return (
+                  <div key={product.id}>
+                    <Product isDarkMode={isDarkMode} product={product} />
+                  </div>
+                )
+              }
+            })}
+          </div>
       </div>
     </div>
   );
